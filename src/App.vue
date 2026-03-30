23eace775dd467b3e2889be465234abc25c4aee7
ppnati33 <ppnati33@gmail.com>
2026-03-30 20:58:18 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <h1 class="text-3xl font-bold text-green-500">Hello</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
